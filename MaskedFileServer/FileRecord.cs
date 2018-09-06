212d97f211f69781689c0ded817ed3330f162125
Chris Rutherford <therealchrisrutherford@gmail.com>
2018-09-05 21:00:59 -0400

    initial commit

---
+++ MaskedFileServer/FileRecord.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace MaskedFileServer
+{
+    public class FileRecord
+    {
+        public String Id {get;set;}
+        public String Path {get;set;}
+        public bool DeleteOnExpiry{get;set;}
+        public DateTime ExpirationDate{get;set;}
+
+        public FileRecord(string _path, int _term = 90, bool _deletionPolicy = false)
+        {
+            Id = new Guid().ToString();
+            Path = _path;
+            ExpirationDate = new DateTime().AddDays(_term);
+            DeleteOnExpiry = _deletionPolicy;
+        }
+    }
+}
---
+++ MaskedFileServer/Wotcha.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MaskedFileServer
+{
+    public static class Wotcha
+    {
+        public static List<FileRecord> FileList;
+
+        public static void Initialize(string _filePath = "//Files//"){
+            FileSystemWatcher fsw = new FileSystemWatcher(_filePath, "*.*");
+            fsw.Created += new FileSystemEventHandler(onCreate);
+            fsw.Deleted += new FileSystemEventHandler(onDelete);
+        }
+
+        private static void onDelete(object sender, FileSystemEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void onCreate(object sender, FileSystemEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
